08b72295a89b6001db04167928bdf91eda62e2d3
iamram2510 <iamram2510@ieee.org>
2019-11-04 18:41:06 +0530

    Added code to return only username and added from db

--- routes/analtyic/index.js
+++ routes/analtyic/index.js
@@ -12,7 +12,7 @@ const {get} = require('../../helpers/conn')
 module.exports = {
   getuserData:(req,res)=>{
     let db = get()
-    db.collection('users').find({}).toArray((err,respData)=>{
+    db.collection('users').find({}).project({username:1,added:1}).toArray((err,respData)=>{
       if(err){
         res.status(500).json([500,"Internal server error",err])
         console.log(err)
@@ -25,13 +25,14 @@ module.exports = {
     let db = get()
 
     const {username} = req.body
-
-    db.collection('details').find({username}).toArray((err,resp)=>{
+    console.log(username)
+    db.collection('details').find({addedBy:username}).toArray((err,resp)=>{
       if(err) {
         res.status(500).json({err})
         console.log(err)    
           return
       }
+      console.log(resp)
       res.status(200).json([200,"ok",resp])
     })
   }
